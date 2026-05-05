57e0eecd4e3b807882dd0e4ea47f5c87586ff212
Aaron Alexander <104107484+HiddenBrilliance@users.noreply.github.com>
2026-05-05 15:20:17 -0500

    added styling to nav

--- react-business-card-content/src/App.jsx
+++ react-business-card-content/src/App.jsx
@@ -1,6 +1,7 @@
 import { useState } from 'react'
 import './App.css'
 import Header from './Header'
+import './Header.css'
 
 function App() {
   const [count, setCount] = useState(0)
--- react-business-card-content/src/Header.jsx
+++ react-business-card-content/src/Header.jsx
@@ -3,16 +3,17 @@ import headshot from './assets/headshot.jpeg'
 
 export default function Header()  {
     return ( <>
-      <header className='header-style'>
+      <header className='header-styles'>
         <img src={headshot} className='headshot' alt="Aaron Alexander" />
-<nav><ul><li>
+<nav><ul className='list-style'><li>
   Home</li>
   <li>Portfolio</li>
   <li>Resume</li>
   <li>Contact Me</li>
-  </ul></nav>
-<h1>Aaron Alexander - Software Engineer</h1>
-  <p>I'm a software engineer with a passion for building web applications. I have experience with React, Node.js, and MongoDB. I'm always looking to learn new technologies and improve my skills.</p>
-      </header>
+  </ul></nav></header>
+  <main><h1>Aaron Alexander - Software Engineer</h1>
+  <p>I'm a software engineer with a passion for building web applications. I have experience with React, Node.js, and MongoDB. I'm always looking to learn new technologies and improve my skills. A musician with a knack for creative solutions to everyday problems, and wine scholar.</p></main>
+
+      
     </> )
 }
